3696d7a54e553e1aa3ca3110e5495361227a6652
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2021-09-25 21:43:57 +0530

    Example of deleting objects having OneToOne unidirectional mapping

---
+++ src/com/hibernate/basics/oneToone/unidirectional/DeleteEmployee.java
@@ -0,0 +1,60 @@
+package com.hibernate.basics.oneToone.unidirectional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.basics.entity.EmployeeOneToOne;
+import com.hibernate.basics.entity.SalaryAccount;
+
+public class DeleteEmployee {
+
+	public static void main(String[] args) {
+
+		// Creating session factory object
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.oneToone.xml")
+				.addAnnotatedClass(EmployeeOneToOne.class).addAnnotatedClass(SalaryAccount.class).buildSessionFactory();
+		try {
+
+			// Creating session object
+			Session session = sessionFactory.getCurrentSession();
+
+			// Starting the Transaction
+			session.beginTransaction();
+
+			// Retrieving Employee object having id 1
+			EmployeeOneToOne retrievedEmployee = session.get(EmployeeOneToOne.class, 1);
+
+			// Deleting retrieved employee object
+			// If retrievedEmployee is null then IllegalArgumentException is thrown
+			// As cascade is all salary account corresponding to id employee having id 1 will also be deleted
+			session.delete(retrievedEmployee);
+
+			System.out.println("Here is the employee which is deleted: " + retrievedEmployee);
+
+			// Committing the transaction
+			session.getTransaction().commit();
+
+			// Creating new session
+			Session newSession = sessionFactory.getCurrentSession();
+
+			// Starting the Transaction
+			newSession.beginTransaction();
+
+			// Deleting email using hql
+			// If no record is found then no exception is thrown
+			// As cascade is all salary account corresponding to id employee having id 2 will also be deleted
+			int rowsUpdated = newSession.createQuery("delete from EmployeeOneToOne where id=2").executeUpdate();
+
+			System.out.println("Number of rows affeted:" + rowsUpdated);
+
+			// Committing the transaction
+			newSession.getTransaction().commit();
+
+		} finally {
+			// Closing the session factory object
+			sessionFactory.close();
+		}
+
+	}
+}
